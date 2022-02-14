2441cc91f4523c0cf439aaadebbf1370a7a26979
Felipe <felipeduram123@gmail.com>
2022-02-14 19:26:17 -0300

    ajustes 14/02/2022 teams

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,7 +1,51 @@
 $(document).ready(function() {    
     $('.articleContent').each(function(){
         //Inicio do script para o componente
-        var ficaAdica = $('.container_fica_a_dica');
+        var boxeAbertura = $('.container_fica_a_dica');
+        boxeAbertura.on('click', function(){
+            console.log('02');
+            var randomId = Math.floor(Math.random() * 100000) + 1;
+            
+            if($(this).find('input').attr('id').length == "0") {
+                console.log('ID alterado');
+                $(this).find('input').attr('id', randomId);
+            } else {
+                console.log('Ja contem ID');
+            }
+            
+            if($(this).find('label').attr('for').length == "0") {
+                console.log('FOR alterado');
+                $(this).find('label').attr('for', randomId);
+            } else {
+                console.log('Ja contem FOR');
+            }
+        });
+        //Fim do script para o componente
+
+        //Inicio do script para o componente
+        var boxeAbertura = $('.container_repertorio_cultural');
+        boxeAbertura.on('click', function(){
+            console.log('02');
+            var randomId = Math.floor(Math.random() * 100000) + 1;
+            
+            if($(this).find('input').attr('id').length == "0") {
+                console.log('ID alterado');
+                $(this).find('input').attr('id', randomId);
+            } else {
+                console.log('Ja contem ID');
+            }
+            
+            if($(this).find('label').attr('for').length == "0") {
+                console.log('FOR alterado');
+                $(this).find('label').attr('for', randomId);
+            } else {
+                console.log('Ja contem FOR');
+            }
+        });
+        //Fim do script para o componente
+
+        //Inicio do script para o componente
+        var ficaAdica = $('.container_saiba_mais');
         ficaAdica.on('click', function(){
             console.log('02');
             var randomId = Math.floor(Math.random() * 100000) + 1;
@@ -21,6 +65,7 @@ $(document).ready(function() {
             }
         });
         //Fim do script para o componente
+
         //Inicio do script para o componente
         var fiqueLigado = $('.css_06_boxe_fique_ligado');
         fiqueLigado.on('click', function(){
