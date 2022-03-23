aa756ecdb3dc097d7ef0294897eb476d431cadbe
Juliano de Arruda Fernandes <juliano.fernandes@sistemapoliedro.com.br>
2022-03-23 10:27:04 -0300

    atualizacao script

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -21,7 +21,48 @@ $(document).ready(function() {
             }
         });
         //Fim do script para o componente
-
+        //Inicio do script para o componente
+        var ficaAdicaTeoria = $('.container_fica_a_dica_teoria');
+        ficaAdicaTeoria.on('click', function(){
+            console.log('02');
+            var randomId = Math.floor(Math.random() * 100000) + 1;
+            
+            if($(this).find('input').attr('id').length == "0") {
+                console.log('ID alterado');
+                $(this).find('input').attr('id', randomId);
+            } else {
+                console.log('Ja contem ID');
+            }
+            
+            if($(this).find('label').attr('for').length == "0") {
+                console.log('FOR alterado');
+                $(this).find('label').attr('for', randomId);
+            } else {
+                console.log('Ja contem FOR');
+            }
+        });
+        //Fim do script para o componente
+        //Inicio do script para o componente
+        var repertorioCulturalTeoria = $('.container_repertorio_cultural_teoria');
+        repertorioCulturalTeoria.on('click', function(){
+            console.log('02');
+            var randomId = Math.floor(Math.random() * 100000) + 1;
+            
+            if($(this).find('input').attr('id').length == "0") {
+                console.log('ID alterado');
+                $(this).find('input').attr('id', randomId);
+            } else {
+                console.log('Ja contem ID');
+            }
+            
+            if($(this).find('label').attr('for').length == "0") {
+                console.log('FOR alterado');
+                $(this).find('label').attr('for', randomId);
+            } else {
+                console.log('Ja contem FOR');
+            }
+        });
+        //Fim do script para o componente
         //Inicio do script para o componente
         var boxeAbertura = $('.container_repertorio_cultural');
         boxeAbertura.on('click', function(){
