dd42cfe47dad62a8b40b24bc7141d76b8d3c0b7c
Wellington Roberto da Silva Paulo <wellington.paulo@sistemapoliedro.com.br>
2022-05-18 12:27:15 -0300

    fast facts 2

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -130,6 +130,28 @@ $(document).ready(function() {
         //Fim do script para o componente
 
         //Inicio do script para o componente
+        var fastFacts = $('.container_fast_facts_exercicios');
+        fastFacts.on('click', function(){
+            console.log('02');
+            var randomId = Math.floor(Math.random() * 100000) + 1;
+            
+            if($(this).find('input').attr('id').length == "0") {
+                console.log('ID alterado');
+                $(this).find('input').attr('id', randomId);
+            } else {
+                console.log('Ja contem ID');
+            }
+            
+            if($(this).find('label').attr('for').length == "0") {
+                console.log('FOR alterado');
+                $(this).find('label').attr('for', randomId);
+            } else {
+                console.log('Ja contem FOR');
+            }
+        });
+        //Fim do script para o componente
+
+        //Inicio do script para o componente
         var takeAtip = $('.container_take_a_tip');
         takeAtip.on('click', function(){
             console.log('02');
